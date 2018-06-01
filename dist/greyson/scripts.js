2fc8f6901cc5a1a6398d5b554d42cbba52f0e8cc
Carol Skelly <carol@iatek.com>
2018-06-01 15:51:06 +0000

    update nav links

--- dist/greyson/scripts.js
+++
@@ -1,6 +0,0 @@
-
-    /* simple scripts file for Themes.guide Bootstrap 4 theme templates */
-
-    // init Bootstrap tooltips & popovers
-    $("[data-toggle=popover]").popover();
-    $("[data-toggle=tooltip]").tooltip();
